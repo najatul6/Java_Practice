1d5c81eb8a7cd7e1852c023a2c727222e28a0824
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-06-05 09:07:39 +0600

    Get outpur Greater than age of 18

---
+++ age.java
@@ -0,0 +1,10 @@
+public class IfExample{
+    public static void main(String[] args) {
+        // defining an 'age' variable
+        int age = 30;
+        // checking the age 
+        if(age>25){
+            System.out.println("Age is greater than 18");
+        }
+    }
+}
